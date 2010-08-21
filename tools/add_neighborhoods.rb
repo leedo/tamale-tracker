9d35b71c423d9ef2795fd48748556c38048b6d04
Clint Ecker <clint@arstechnica.com>
2010-08-21 10:12:21 -0500

    Reliably getting/caching results from yelp

--- tools/add_neighborhoods.rb
+++ tools/add_neighborhoods.rb
@@ -35,25 +35,31 @@ y = Yelp.new(key)
 
 place_cache = {}
 
-locations_data[0...5].each do |l|
+locations_data[0...10].each do |l|
   # We can't do anything without a street location
   if l['bar']['street']
     street = l['bar']['street'] + ", Chicago, IL"
     name = l['bar']['name']
     # Have we already asked yelp about this place?
     neighborhood = l['bar']['neighborhood'] ? l['bar']['neighborhood'] : nil
-    continue if neighborhood
+    if neighborhood
+      puts "Neighborhood for #{name} already in stored in JSON (#{neighborhood})."
+      next
+    end
     # Is it in our session yelp cache?
     if place_cache[name]
-      continue if place_cache[name]['bar']['neighborhood']
+      puts "We've already asked yelp for #{name}'s neighborhood (#{place_cache[name]['bar']['neighborhood']}) this session."
+      next if place_cache[name]['bar']['neighborhood']
     end
     # Ok, so there's no neighborhood for this place in the JSON
     #  nor have we got a neighborhood for it from Yelp during
     #  this session, lets ask yelp about it.
     neighborhood = y.neighborhood_search(street)
+    # Update huge locations structure
     l['bar']['neighborhood'] = neighborhood
+    puts "Asked yelp for neighborhood for #{name} and got #{neighborhood}, storing."
+    # Cache this location name for this session to cut down
+    #   on yelp requests
     place_cache[name] = l
   end
 end
-
-puts place_cache.inspect
